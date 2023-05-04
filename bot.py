7e431cec10c691e179eea0c1bfe80358d658eea1
User_1305 <User_1305>
2023-05-04 11:48:26 -0700

    comments & message.content

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from os import environ
 from dotenv import load_dotenv
 import discord
 
-# read variables from .env file
+# read variables from .env file using the dotenv library
 load_dotenv()
 
 # set up intents
@@ -22,12 +22,18 @@ async def on_ready():
 # event handler for when a message arrives
 @client.event
 async def on_message(message):
+    # Does not act if the message is sent by the bot itself
     if message.author == client.user:
         return
 
+    # If a message that has been sent starts with hello, than reply with a hello
     if message.content.startswith("$hello"):
         await message.channel.send("Hello!")
 
+    # If a message that has been sent that contains the words "good job" reply with a "Thank You"
+    if "good job" in message.content:
+        await message.channel.send("Thank You!")
 
-# start the client
+
+# start the client using the en
 client.run(environ["DISCORD_TOKEN"])
