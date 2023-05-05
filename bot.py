470ed242de2af01c54120671c04b2655f3e8da1b
User_1305 <User_1305>
2023-05-05 14:30:10 -0700

    roll the die

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from os import environ
 from dotenv import load_dotenv
 import discord
+import datetime
+import random
+
 
 # read variables from .env file using the dotenv library
 load_dotenv()
@@ -34,6 +37,25 @@ async def on_message(message):
     if "good job" in message.content:
         await message.channel.send("Thank You!")
 
+    if message.content.startswith("$date") or message.content.startswith("$time"):
+        current_time = datetime.datetime.now()
+        await message.channel.send(f"Current date and time: {current_time}")
 
-# start the client using the en
+    if message.content.startswith("$roll"):
+        try:
+            sides = int(message.content.split()[1])
+            if sides >= 100000:
+                await message.channel.send(f"Sorry, that is too many sides.")
+            elif sides < 2:
+                await message.channel.send(f"A die needs to have at least two sides.")
+            else:
+                result = random.randint(1, sides)
+                await message.channel.send(f"🎲 A {sides} sided die landed on {result}.")
+        except (ValueError, IndexError):
+            await message.channel.send(
+                f"Please provide a valid number of sides for the die."
+            )
+
+
+# start the client using the dotenv package
 client.run(environ["DISCORD_TOKEN"])
