bfadcb0900ccdde25ede461789cbea052b56f983
User_1305 <User_1305>
2023-05-08 18:00:20 -0700

    calendar command

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 import discord
 import datetime
 import random
-
+from icalendar.cal import Calendar, Component
+import datetime
 
 # read variables from .env file using the dotenv library
 load_dotenv()
@@ -56,6 +57,39 @@ async def on_message(message):
                 f"Please provide a valid number of sides for the die."
             )
 
+    if message.content.startswith("$events"):
+        ics_file_path = r"calendar.ics"
+        # Get the current date and time using the datetime library
+        now = datetime.datetime.now()
+        # Example of how to add days to a date
+        one_week_from_now = now + datetime.timedelta(weeks=1)
+        upcoming_events = [["Start", "End", "Summary"]]
 
-# start the client using the dotenv package
+        # Read the .ics file named 'calendar.ics' into a 'icalendar.Calendar' object called cal.
+        with open(ics_file_path, "r") as file:
+            cal = Component.from_ical(file.read())
+
+        # Iterate over each component (event) with the name of VEVENT in the .ics file using the cal.walk() generator.
+        for component in cal.walk("VEVENT"):
+            event_start = component.decoded("dtstart")
+            event_end = component.decoded("dtend")
+            summary = component.get("summary")
+
+            if now.date() <= event_start:
+                upcoming_events.append([str(event_start), str(event_end), str(summary)])
+
+        # Display the next three events (if any)
+        NUM_OF_EVENTS = 3
+        msg = "```"
+        msg += "Next 3 Events (yyyy-mm-dd): \n"
+        for i in range(NUM_OF_EVENTS + 1):
+            msg += f"{upcoming_events[i][0]:10} - {upcoming_events[i][1]:10} - {upcoming_events[i][2]}\n"
+            if i == 0:
+                msg += "\n"
+        msg += "```"
+
+        await message.channel.send(msg)
+
+
+# the dotenvpackage
 client.run(environ["DISCORD_TOKEN"])
